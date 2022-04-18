5911496ad88b57aa0a3314cb5d24de98754f60e0
pithiyamilan <milan.pithiya@acuteinformatics.in>
2022-04-18 12:04:01 +0530

    reject related changes

--- backend/controllers/queueMessage.ts
+++ backend/controllers/queueMessage.ts
@@ -26,3 +26,39 @@ export const camIntiate = catchAsyncErrors(async (req: any, res: any, next: any)
         status: "0", responseData: { message: "CAM Initiate successfully." }
     });
 });
+
+export const camIntiateTest = catchAsyncErrors(async (req: any, res: any, next: any) => {
+    const requestExchange = "AgentForCAMRequest";
+    const requestExchangeRouting = "CAMRequest";
+
+    const requestDelayExchange = "AgentForDelayCAMRequest";
+    const requestDelayExchangeRouting = "CAMDelayRequest";
+
+    const failedRequestExchange = "CAMFailedRequestQueue";
+    const failedRequestExchangeRouting = "CAMFailed";
+
+    const camRequestQueue = "CAMRequestQueue";
+    const camFailedRequestQueue = "CAMFailedRequestQueue";
+
+    const qConnection = await queueConnection(process.env.RABBITMQ_URL);
+    if (!qConnection) { 
+         return next(new errorHandler(errorMessage, "99", "-99", "RabbitMQ Connection Refused.","Error while RabbitMQ connection.", 400));
+    }
+    const qChannel = await queueChannel(qConnection);
+    if (!qChannel) { 
+        return next(new errorHandler(errorMessage, "99", "-99", "RabbitMQ Channel Connection Refused.","Error while RabbitMQ Channel connection.", 400));
+    };
+    
+    await qChannel.assertQueue(requestExchange, "direct");
+    await qChannel.assertQueue(requestDelayExchange, "direct");
+    await qChannel.assertQueue(failedRequestExchange, "direct");
+
+    
+    
+
+    await qChannel.close();
+    await qConnection.close();
+    res.json({
+        status: "0", responseData: { message: "CAM Initiate successfully." }
+    });
+});
